c70fc34240896c23cdf3c5dd603463aa0452662d
Mandy <yymgu@edu.uwaterloo.ca>
2018-05-08 17:30:06 -0400

    changes to readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Computer-Vision-on-a-Budget
+
+The simplest, hackish way of constructing a seemingly live classifier.
+
+Done in under 20 lines of code. 
+
--- cv on budget.py
+++ cv on budget.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 from keras.applications import imagenet_utils, VGG19
 
-model = VGG19(weights='imagenet')
+model = VGG19(weights='imagenet') # load weights
+
+''' < 20 lines of code starts here '''
 
 cam = cv2.VideoCapture(0) # Open webcam 
 
@@ -18,7 +20,7 @@ while True:
     frame_pred = frame_pred.reshape((1, ) + frame_pred.shape)
     frame_pred = imagenet_utils.preprocess_input(frame_pred)
     predictions = model.predict(frame_pred)
-    (imageID, label, score) = imagenet_utils.decode_predictions(predictions)[0][0]
+    (label_id, label, score) = imagenet_utils.decode_predictions(predictions)[0][0]
 
     cv2.putText(frame, "%s with Probability: %.2f" % (label, score), (25, 25), cv2.FONT_HERSHEY_DUPLEX, 0.9, (0, 0, 250), 2)
     cv2.imshow('Computer Vision on a Budget', frame)
